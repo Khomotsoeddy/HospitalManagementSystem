2a805e663468837d39520f9d0b4c745086d90b57
Khomotso <khomotsoeddy@gmail.com>
2022-08-16 21:23:44 +0200

    added biometric authentication

---
+++ app/src/main/java/za/ac/tut/hospitalmanagementsystem/BiometricActivity.kt
@@ -0,0 +1,82 @@
+package za.ac.tut.hospitalmanagementsystem
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import za.ac.tut.hospitalmanagementsystem.admin.AdminActivity
+import za.ac.tut.hospitalmanagementsystem.doctor.DoctorActivity
+import za.ac.tut.hospitalmanagementsystem.patient.PatientActivity
+import java.util.concurrent.Executor
+
+class BiometricActivity : AppCompatActivity() {
+
+    // fingerprint or face
+    private lateinit var executor: Executor
+    private lateinit var biometricPrompt: androidx.biometric.BiometricPrompt
+    private lateinit var promptInfo: androidx.biometric.BiometricPrompt.PromptInfo
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_biometric)
+
+        val radioPatient = findViewById<RadioButton>(R.id.radioPatient)
+        val radioAdmin = findViewById<RadioButton>(R.id.radioAdmin)
+        val radioDoctor = findViewById<RadioButton>(R.id.radioDoctor)
+        val buttonLogin = findViewById<Button>(R.id.buttonLogin)
+
+        executor = ContextCompat.getMainExecutor(this)
+        biometricPrompt = androidx.biometric.BiometricPrompt(this,executor,object:androidx.biometric.BiometricPrompt.AuthenticationCallback(){
+
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                super.onAuthenticationError(errorCode, errString)
+                Toast.makeText(this@BiometricActivity, "Error! $errString", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onAuthenticationSucceeded(result: androidx.biometric.BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(result)
+
+                ///Toast.makeText(this@LoginActivity,result.toString(),Toast.LENGTH_SHORT).show()
+                if (radioAdmin.isChecked) {
+                    val intent = Intent(this@BiometricActivity, AdminActivity::class.java)
+                    startActivity(intent)
+                    Toast.makeText(this@BiometricActivity, "Successful", Toast.LENGTH_SHORT).show()
+                } else if (radioPatient.isChecked) {
+                    val intent = Intent(this@BiometricActivity, PatientActivity::class.java)
+                    startActivity(intent)
+                    Toast.makeText(this@BiometricActivity, "Successful", Toast.LENGTH_SHORT).show()
+                } else if (radioDoctor.isChecked) {
+                    val intent = Intent(this@BiometricActivity, DoctorActivity::class.java)
+                    startActivity(intent)
+                    Toast.makeText(this@BiometricActivity, "Successful", Toast.LENGTH_SHORT).show()
+                }else {
+                    Toast.makeText(
+                        this@BiometricActivity,
+                        "Please choose your role",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+            override fun onAuthenticationFailed() {
+                super.onAuthenticationFailed()
+                Toast.makeText(this@BiometricActivity, "Failed", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        promptInfo = androidx.biometric.BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric Authentication")
+            .setSubtitle("Login using fingerprint or face")
+            .setNegativeButtonText("Cancel")
+            .build()
+
+        //if(radioAdmin.isChecked || radioPatient.isChecked || radioDoctor.isChecked) {
+        buttonLogin.setOnClickListener {
+            biometricPrompt.authenticate(promptInfo)
+
+        }
+    }
+}
--- app/src/main/java/za/ac/tut/hospitalmanagementsystem/LoginActivity.kt
+++ app/src/main/java/za/ac/tut/hospitalmanagementsystem/LoginActivity.kt
@@ -5,30 +5,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.RadioButton
+import android.widget.TextView
 import android.widget.Toast
-import androidx.core.content.ContextCompat
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import za.ac.tut.hospitalmanagementsystem.admin.AdminActivity
 import za.ac.tut.hospitalmanagementsystem.doctor.DoctorActivity
 import za.ac.tut.hospitalmanagementsystem.patient.PatientActivity
-import java.util.concurrent.Executor
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var database : DatabaseReference
-
-    // fingerprint or face
-    lateinit var executor: Executor;
-    lateinit var biometricPrompt: androidx.biometric.BiometricPrompt
-    lateinit var promptInfo: androidx.biometric.BiometricPrompt.PromptInfo
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         val buttonLogin = findViewById<Button>(R.id.buttonLogin)
+
+        val textView = findViewById<TextView>(R.id.textView)
+
+        textView.setOnClickListener {
+            val intent = Intent(this@LoginActivity, BiometricActivity::class.java)
+            startActivity(intent)
+        }
 
         val radioPatient = findViewById<RadioButton>(R.id.radioPatient)
         val radioAdmin = findViewById<RadioButton>(R.id.radioAdmin)
@@ -37,64 +38,7 @@ class LoginActivity : AppCompatActivity() {
         val textInputEditTextId = findViewById<TextInputEditText>(R.id.textInputEditTextId)
         val textInputEditTextPassword = findViewById<TextInputEditText>(R.id.textInputEditTextPassword)
 
-        executor = ContextCompat.getMainExecutor(this)
-        biometricPrompt = androidx.biometric.BiometricPrompt(this@LoginActivity,executor,object:androidx.biometric.BiometricPrompt.AuthenticationCallback(){
-
-            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
-                super.onAuthenticationError(errorCode, errString)
-                Toast.makeText(this@LoginActivity, "Error! $errString",Toast.LENGTH_SHORT).show()
-            }
-
-            override fun onAuthenticationSucceeded(result: androidx.biometric.BiometricPrompt.AuthenticationResult) {
-                super.onAuthenticationSucceeded(result)
-
-                ///Toast.makeText(this@LoginActivity,result.toString(),Toast.LENGTH_SHORT).show()
-                if(radioAdmin.isChecked){
-                    val intent = Intent(this@LoginActivity, AdminActivity::class.java)
-                    startActivity(intent)
-                    Toast.makeText(this@LoginActivity, "Successful",Toast.LENGTH_SHORT).show()
-                }
-                if(radioPatient.isChecked){
-                    val intent = Intent(this@LoginActivity, PatientActivity::class.java)
-                    startActivity(intent)
-                    Toast.makeText(this@LoginActivity, "Successful",Toast.LENGTH_SHORT).show()
-                }
-                if(radioDoctor.isChecked){
-                    val intent = Intent(this@LoginActivity, DoctorActivity::class.java)
-                    startActivity(intent)
-                    Toast.makeText(this@LoginActivity, "Successful",Toast.LENGTH_SHORT).show()
-                }
-
-            }
-
-            override fun onAuthenticationFailed() {
-                super.onAuthenticationFailed()
-                Toast.makeText(this@LoginActivity, "Failed",Toast.LENGTH_SHORT).show()
-            }
-        })
-
-        promptInfo = androidx.biometric.BiometricPrompt.PromptInfo.Builder()
-            .setTitle("Biometric Authentication")
-            .setSubtitle("Login using fingerprint or face")
-            .setNegativeButtonText("Cancel")
-            .build()
-
-
-        /*if(radioAdmin.isChecked){
-
-            Toast.makeText(this@LoginActivity,promptInfo.negativeButtonText,Toast.LENGTH_SHORT).show()
-        }lse if(radioPatient.isChecked){
-            biometricPrompt.authenticate(promptInfo)
-
-            Toast.makeText(this@LoginActivity,promptInfo.negativeButtonText,Toast.LENGTH_SHORT).show()
-        }else if(radioDoctor.isChecked){
-            biometricPrompt.authenticate(promptInfo)
-
-            Toast.makeText(this@LoginActivity,promptInfo.negativeButtonText,Toast.LENGTH_SHORT).show()
-        }*/
-
         buttonLogin.setOnClickListener {
-            biometricPrompt.authenticate(promptInfo)
             if(radioAdmin.isChecked){
                 goToAdminPage(textInputEditTextId,textInputEditTextPassword)
             }else if(radioPatient.isChecked){
